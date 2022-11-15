5c3a4ac61467242f22129130224e183a8727f2d3
luke-github <118205955+luke-github@users.noreply.github.com>
2022-11-15 13:55:10 +0000

    scroll thing and stars rotate in group

--- WD_project/main.js
+++ WD_project/main.js
@@ -2,6 +2,7 @@ import './style.css'
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
+// necessary setup
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -12,9 +13,14 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
+camera.position.setZ(0);
 let cameraY = 0;
 let isCamMoveUp = true;
+
+const wireframeMaterial = new THREE.MeshBasicMaterial({color: 0xffffff, wireframe: true});
+
+const starsGroup = new THREE.Group();
+//
 
 function switchCameraYMovement(y) {
   if (y >= 30) {
@@ -26,9 +32,7 @@ function switchCameraYMovement(y) {
 } 
 
 
-
-const wireframeMaterial = new THREE.MeshBasicMaterial({color: 0xffffff, wireframe: true});
-
+//orbit tings
 const orbit = new OrbitControls(camera, renderer.domElement);
 orbit.target = new THREE.Vector3(0,0,0);
 orbit.minDistance = 35;
@@ -36,7 +40,7 @@ orbit.zoomSpeed = 0.3;
 orbit.autoRotate = true;
 orbit.autoRotateSpeed = 2.0;
 orbit.enabled = false;
-
+//
 
 function addStarsS() {
   const dodecaGeoSmall = new THREE.IcosahedronGeometry(.1, 0);
@@ -46,7 +50,8 @@ function addStarsS() {
   const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100) );
 
   star.position.set(x, y, z);
-  scene.add(star);
+  starsGroup.add(star);
+  // scene.add(star);
 
 }
 
@@ -58,7 +63,8 @@ function addStarsM() {
   const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100) );
 
   star.position.set(x, y, z);
-  scene.add(star);
+  starsGroup.add(star);
+  // scene.add(star);
 
 }
 
@@ -70,28 +76,26 @@ function addStarsL() {
   const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100) );
 
   star.position.set(x, y, z);
-  scene.add(star);
+  starsGroup.add(star);
+  // scene.add(star);
 }
+scene.add(starsGroup);
 
 
-
-
+//planetry tings
 const planetGeo = new THREE.SphereGeometry(3.8, 25, 13, 3.141592653589793, /*6.283185307179586*/3.141592653589793, 0, 3.141592653589793);
 const planetGeo2 = new THREE.SphereGeometry(3.8, 25, 13, 0, 3.141592653589793, 0, 3.141592653589793)
 const planet = new THREE.Mesh(planetGeo, wireframeMaterial);
 const planet2 = new THREE.Mesh(planetGeo2, wireframeMaterial);
 
-// const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(45) );
-
 planet.position.set(4, -1, 9);
 planet2.position.set(4, -1, 9);
 scene.add(planet, planet2);
-
+//
 
 Array(25).fill().forEach(addStarsL);
 Array(65).fill().forEach(addStarsM);
 Array(110).fill().forEach(addStarsS);
-
 
 
 function animate() {
@@ -102,7 +106,7 @@ function animate() {
   planet2.rotation.x += .005
   planet2.rotation.y += 0.0075
 
-  
+  starsGroup.rotateY(0.0005);
 
   switchCameraYMovement(cameraY);
 
@@ -131,7 +135,9 @@ animate();
 
 function moveCamera() {
   const t = document.body.getBoundingClientRect().top;
-
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.position.y = t * -0.0002;
 }
 
 document.body.onscroll = moveCamera;
