05f89c601f169c9a0a64d0881fa2ca4b98c447cd
luke-github <118205955+luke-github@users.noreply.github.com>
2022-11-15 01:38:58 +0000

    qpweoiurqwopieru

--- WD_project/main.js
+++ WD_project/main.js
@@ -1,23 +1,123 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const scene = new THREE.Scene();
 
-setupCounter(document.querySelector('#counter'))
+const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+let cameraY = 0;
+let isCamMovePos = true;
+
+function switchCameraYMovement(y) {
+  if (y >= 30) {
+    return false;
+  }
+  if (y <= -30) {
+    return true
+  }
+} 
+
+
+
+const wireframeMaterial = new THREE.MeshBasicMaterial({color: 0xffffff, wireframe: true});
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+orbit.target = new THREE.Vector3(0,0,0);
+orbit.minDistance = 35;
+orbit.zoomSpeed = 0.3;
+orbit.autoRotate = true;
+orbit.autoRotateSpeed = 2.0;
+orbit.enabled = false;
+
+
+function addStarsS() {
+  const dodecaGeoSmall = new THREE.IcosahedronGeometry(.1, 0);
+  const material = new THREE.MeshBasicMaterial({color: 0xFFFFFF, wireframe: true});
+  const star = new THREE.Mesh(dodecaGeoSmall, material);
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100) );
+
+  star.position.set(x, y, z);
+  scene.add(star);
+
+}
+
+function addStarsM() {
+  const dodecaGeoMedium = new THREE.DodecahedronGeometry(.3, 1);
+  const material = new THREE.MeshBasicMaterial({color: 0xFFFFFF, wireframe: true});
+  const star = new THREE.Mesh(dodecaGeoMedium, material);
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100) );
+
+  star.position.set(x, y, z);
+  scene.add(star);
+
+}
+
+function addStarsL() {
+  const dodecaGeoLarge = new THREE.DodecahedronGeometry(.5, 2);
+  const material = new THREE.MeshBasicMaterial({color: 0xFFFFFF, wireframe: true});
+  const star = new THREE.Mesh(dodecaGeoLarge, material);
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100) );
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+
+const planetGeo = new THREE.SphereGeometry(3.8, 25, 13, 3.141592653589793, /*6.283185307179586*/3.141592653589793, 0, 3.141592653589793);
+const planetGeo2 = new THREE.SphereGeometry(3.8, 25, 13, 0, 3.141592653589793, 0, 3.141592653589793)
+const planet = new THREE.Mesh(planetGeo, wireframeMaterial);
+const planet2 = new THREE.Mesh(planetGeo2, wireframeMaterial);
+
+// const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(45) );
+
+planet.position.set(4, -1, 9);
+planet2.position.set(4, -1, 9);
+scene.add(planet, planet2);
+
+
+Array(25).fill().forEach(addStarsL);
+Array(65).fill().forEach(addStarsM);
+Array(110).fill().forEach(addStarsS);
+
+
+function animate() {
+  requestAnimationFrame(animate);
+  
+  planet.rotation.x += 0.01;
+  planet.rotation.y += 0.015;
+  planet2.rotation.x += 0.01;
+  planet2.rotation.y += 0.015;
+
+  switchCameraYMovement(cameraY);
+
+  
+  if (isCamMovePos) {
+    cameraY += .03;
+    if (cameraY >= 30) {
+      isCamMovePos = switchCameraYMovement(cameraY);
+    }
+  }
+  if (!isCamMovePos) {
+    cameraY -= .03;
+    if (cameraY <= -30) {
+      isCamMovePos = switchCameraYMovement(cameraY);
+    }
+  }
+  
+  camera.position.setY(cameraY);
+
+  orbit.update();
+  renderer.render(scene,camera);
+}
+animate();
